bf7df89da4930006fd29bf3352e6f6b35f98f27e
Boutsman <boutsman@gmail.com>
2016-02-04 10:40:52 +0100

    Btn release added, right file this time

--- ArduinoStrategicCommander.ino
+++ ArduinoStrategicCommander.ino
@@ -37,28 +37,42 @@ void clickBtn(int btn) {
   val[btn-2] = digitalRead(btn);
 
   if (val[btn-2] == 1 && lastVal[btn-2] == 0) {
-    winampCtrl(0);
+    if (mode == 0) {
+      hotkeyCtrl(btn+7);
+    }
+    else if (mode == 1) {
+      hotkeyCtrl(btn + 14+7);
+    }
+    else if (mode == 2) {
+      hotkeyCtrl(0);
+    }
+    else if (mode == 3) {
+      hotkeyCtrl(btn + 35+7);
+    }
+    else {
+      hotkeyCtrl(0);
+    }
   }
   else if (val[btn-2] == 0 && lastVal[btn-2] == 1) {
     if (mode == 0) {
-      winampCtrl(btn);
+      hotkeyCtrl(btn);
     }
     else if (mode == 1) {
-      winampCtrl(btn + 8);
+      hotkeyCtrl(btn + 14);
     }
     else if (mode == 2) {
-      winampCtrl(btn + 16);
+      hotkeyCtrl(btn + 28);
     }
     else if (mode == 3) {
-      winampCtrl(btn + 24);
+      hotkeyCtrl(btn + 35);
     }
     else {
-      winampCtrl(0);
+      hotkeyCtrl(0);
     }
   }
   else
   {
-    winampCtrl(0);
+    hotkeyCtrl(0);
   }
   lastVal[btn-2] = val[btn-2];
 }
@@ -68,7 +82,7 @@ void nextMode(int btn) {
   val[btn-2] = digitalRead(btn);
 
   if (val[btn] == 1 && lastVal[btn] == 0) {
-    winampCtrl(0);
+    hotkeyCtrl(0);
   }
   else if (val[btn-2] == 0 && lastVal[btn-2] == 1) {
     if (mode < (amountOfModes - 1)) {
@@ -86,13 +100,13 @@ void nextMode(int btn) {
   }
   else
   {
-    winampCtrl(0);
+    hotkeyCtrl(0);
   }
   lastVal[btn-2] = val[btn-2];
 }
 
 //CASE-structure to send each keycombination
-void winampCtrl(int state) {
+void hotkeyCtrl(int state) {
   switch (state) {
     //Commands for WINAMP
     case 2:
@@ -101,7 +115,6 @@ void winampCtrl(int state) {
       Keyboard.press('z');
       Serial.println("rev");
       delay(100);
-      Keyboard.releaseAll();
       break;
     case 3:
       Keyboard.press(KEY_LEFT_CTRL);
@@ -109,15 +122,13 @@ void winampCtrl(int state) {
       Keyboard.press(KEY_PAGE_UP);
       Serial.println("vol+");
       delay(100);
-      Keyboard.releaseAll();
       break;
     case 4:
       Keyboard.press(KEY_LEFT_CTRL);
       Keyboard.press(KEY_LEFT_SHIFT);
       Keyboard.press(KEY_PAGE_DOWN);
-      Serial.println("vol-");
+      Serial.println("vol+");
       delay(100);
-      Keyboard.releaseAll();
       break;
     case 5:
       Keyboard.press(KEY_LEFT_CTRL);
@@ -125,7 +136,6 @@ void winampCtrl(int state) {
       Keyboard.press('b');
       Serial.println("ffw");
       delay(100);
-      Keyboard.releaseAll();
       break;
     case 6:
       Keyboard.press(KEY_LEFT_CTRL);
@@ -133,7 +143,6 @@ void winampCtrl(int state) {
       Keyboard.press('c');
       Serial.println("pause");
       delay(100);
-      Keyboard.releaseAll();
       break;
     case 7:
       Keyboard.press(KEY_LEFT_CTRL);
@@ -141,7 +150,6 @@ void winampCtrl(int state) {
       Keyboard.press('x');
       Serial.println("play");
       delay(100);
-      Keyboard.releaseAll();
       break;
     case 8:
       Keyboard.press(KEY_LEFT_CTRL);
@@ -149,80 +157,137 @@ void winampCtrl(int state) {
       Keyboard.press('v');
       Serial.println("stop");
       delay(100);
-      Keyboard.releaseAll();
       break;
-
-    //Commands for "a sunless sea"
+      
+    case 9:
+      Keyboard.release(KEY_LEFT_CTRL);
+      Keyboard.release(KEY_LEFT_SHIFT);
+      Keyboard.release('z');
+      Serial.println("rev2");
+      break;
     case 10:
-      Keyboard.press('a');
-      Serial.println("Steer left");
-      delay(100);
-      Keyboard.releaseAll();
+      Keyboard.release(KEY_LEFT_CTRL);
+      Keyboard.release(KEY_LEFT_SHIFT);
+      Keyboard.release(KEY_PAGE_UP);
+      Serial.println("vol+2");
       break;
     case 11:
-      Keyboard.press('w');
-      Serial.println("Power+");
-      delay(100);
-      Keyboard.releaseAll();
+      Keyboard.release(KEY_LEFT_CTRL);
+      Keyboard.release(KEY_LEFT_SHIFT);
+      Keyboard.release(KEY_PAGE_DOWN);
+      Serial.println("vol-2");
       break;
     case 12:
-      Keyboard.press('s');
-      Serial.println("Power-");
-      delay(100);
-      Keyboard.releaseAll();
+      Keyboard.release(KEY_LEFT_CTRL);
+      Keyboard.release(KEY_LEFT_SHIFT);
+      Keyboard.release('b');
+      Serial.println("ffw2");
       break;
     case 13:
-      Keyboard.press('d');
-      Serial.println("Steer right");
-      delay(100);
-      Keyboard.releaseAll();
+      Keyboard.release(KEY_LEFT_CTRL);
+      Keyboard.release(KEY_LEFT_SHIFT);
+      Keyboard.release('c');
+      Serial.println("pause2");
       break;
     case 14:
-      Keyboard.press('1');
-      Serial.println("Fire");
-      delay(100);
-      Keyboard.releaseAll();
+      Keyboard.release(KEY_LEFT_CTRL);
+      Keyboard.release(KEY_LEFT_SHIFT);
+      Keyboard.release('x');
+      Serial.println("play2");
       break;
     case 15:
-      Keyboard.press('l');
-      Serial.println("Toggle lights");
-      delay(100);
-      Keyboard.releaseAll();
-      break;
-    case 16:
-      Keyboard.press('g');
-      Serial.println("Open journal");
-      delay(100);
-      Keyboard.releaseAll();
+      Keyboard.release(KEY_LEFT_CTRL);
+      Keyboard.release(KEY_LEFT_SHIFT);
+      Keyboard.release('v');
+      Serial.println("stop2");
       break;
 
-    //Commands for "Inkscape"
+    //Commands for "Sunless Sea"
+    case 16:
+      Keyboard.press('a');
+      Serial.println("left");
+      delay(100);
+      break;
+    case 17:
+      Keyboard.press('w');
+      Serial.println("power+");
+      delay(100);
+      break;
     case 18:
+      Keyboard.press('s');
+      Serial.println("power-");
+      delay(100);
+      break;
+    case 19:
+      Keyboard.press('d');
+      Serial.println("right");
+      delay(100);
+      break;
+    case 20:
+      Keyboard.press('1');
+      Serial.println("fire");
+      delay(100);
+      break;
+    case 21:
+      Keyboard.press('l');
+      Serial.println("lights");
+      delay(100);
+      break;
+    case 22:
+      Keyboard.press('g');
+      Serial.println("journal");
+      delay(100);
+      break;
+      
+    case 23:
+      Keyboard.release('a');
+      break;
+    case 24:
+      Keyboard.release('w');
+      break;
+    case 25:
+      Keyboard.release('s');
+      break;
+    case 26:
+      Keyboard.release('d');
+      break;
+    case 27:
+      Keyboard.release('1');
+      break;
+    case 28:
+      Keyboard.release('l');
+      break;
+    case 29:
+      Keyboard.release('g');
+      break;
+
+       //Commands for "Inkscape"
+    case 30:
       Keyboard.press(KEY_F5);
       Serial.println("Circles");
       delay(100);
       Keyboard.releaseAll();
       break;
-    case 19:
+    case 31:
       Keyboard.press(KEY_F8);
       Serial.println("Text");
       delay(100);
       Keyboard.releaseAll();
       break;
-    case 20:
+    case 32:
       Keyboard.press(KEY_LEFT_SHIFT);
       Keyboard.press(KEY_F6);
       Serial.println("Lines");
       delay(100);
       Keyboard.releaseAll();
       break;
-    case 21:
+    case 33:
       Keyboard.press(KEY_F4);
       Serial.println("Squares");
       delay(100);
       Keyboard.releaseAll();
       break;
-    case 22:
+    case 34:
       Keyboard.press(KEY_LEFT_CTRL);
       Keyboard.press(KEY_LEFT_SHIFT);
       Keyboard.press('f');
@@ -230,7 +295,7 @@ void winampCtrl(int state) {
       delay(100);
       Keyboard.releaseAll();
       break;
-    case 23:
+    case 35:
       Keyboard.press(KEY_LEFT_CTRL);
       Keyboard.press(KEY_LEFT_SHIFT);
       Keyboard.press('l');
@@ -238,53 +303,9 @@ void winampCtrl(int state) {
       delay(100);
       Keyboard.releaseAll();
       break;
-    case 24:
+    case 36:
       Keyboard.press(KEY_ESC);
       Serial.println("Return to canvas");
-      delay(100);
-      Keyboard.releaseAll();
-      break;
-
-    //Commands for "OpenSCAD"
-    case 26:
-      Keyboard.print("sphere(radius | d=diameter)");
-      Serial.println("Sphere");
-      delay(100);
-      Keyboard.releaseAll();
-      break;
-    case 27:
-      Keyboard.print("translate([x,y,z])");
-      Serial.println("Translate");
-      delay(100);
-      Keyboard.releaseAll();
-      break;
-    case 28:
-      Keyboard.print("cylinder(h,r1|d1,r2|d2,center)");
-      Serial.println("Cylinder");
-      delay(100);
-      Keyboard.releaseAll();
-      break;
-    case 29:
-      Keyboard.print("cube([width,depth,height], center)");
-      Serial.println("Cube");
-      delay(100);
-      Keyboard.releaseAll();
-      break;
-    case 30:
-      Keyboard.print("union()");
-      Serial.println("Union");
-      delay(100);
-      Keyboard.releaseAll();
-      break;
-    case 31:
-      Keyboard.print("difference()");
-      Serial.println("Difference");
-      delay(100);
-      Keyboard.releaseAll();
-      break;
-    case 32:
-      Keyboard.print("intersection()");
-      Serial.println("Intersection");
       delay(100);
       Keyboard.releaseAll();
       break;
